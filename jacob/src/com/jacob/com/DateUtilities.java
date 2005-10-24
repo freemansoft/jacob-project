663505c0ce3d45daf171ef48b15ccc23f8e8d3e8
clay_shooter <>
2005-10-24 00:10:14 +0000

    converted from custom Jacob license to GNU LGPL to make license terms easier to understand.

--- jacob/src/com/jacob/com/DateUtilities.java
+++ jacob/src/com/jacob/com/DateUtilities.java
@@ -1,10 +1,39 @@
+/*
+ * Copyright (c) 1999-2004 Sourceforge JACOB Project.
+ * All rights reserved. Originator: Dan Adler (http://danadler.com).
+ * Get more information about JACOB at www.sourceforge.net/jacob-project
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
 package com.jacob.com;
 
 import java.util.Calendar;
 import java.util.Date;
 
+/**
+ * java / windows date conversion utilities
+ * @author joe
+ *
+ */
 public class DateUtilities {
 
+	/**
+	 * converts a windows time to a Java Date Object
+	 * @param comTime
+	 * @return
+	 */
 	static public Date convertWindowsTimeToDate(double comTime){
 		return new Date(convertWindowsTimeToMilliseconds (comTime));
 	}
